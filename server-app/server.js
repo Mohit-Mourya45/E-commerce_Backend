f086cd9216ccc025e088fa7a49ef069630afa46d
Mohit-Mourya45 <0808cl241117.ies@ipsacademy.org>
2026-06-28 21:58:46 +0530

    update api connection

--- server-app/server.js
+++ server-app/server.js
@@ -19,12 +19,15 @@ const paymentdetailsRoute = require('./admin/bills/paymentdetails.route.js');
 const paymentRoute = require('./payment.js')
 
 app.use(cors({
-    origin: [
-        "http://localhost:3000",
-        "https://e-commerce-backend-pi-black.vercel.app"
-    ],
-    credentials: true
+  origin: [
+    "http://localhost:3000",
+    "https://e-commerce-frontend-sage-one.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+
 }));
+app.options("*", cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use('/state',stateRoute);
